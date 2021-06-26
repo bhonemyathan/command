e76da75076b6c7fd0d80a8036a71756ec1553395
noobprogrammerD4V8ll <83602194+noobprogrammerD4V8ll@users.noreply.github.com>
2021-06-26 15:11:46 +0000

    Add files via upload

---
+++ command.sh
@@ -0,0 +1,16 @@
+clear
+figlet Command Injection
+echo -e '\e[1;31m [+]\e[0mInstalling PHP\e[1;31m[+] \e[0m'
+echo ""
+pkg install php
+echo -e '\e[1;31m [+]\e[0mInstalling unzip\e[1;31m[+] \e[0m'
+pkg install unzip
+echo ""
+echo -e '\e[1;31m [+]\e[0mPlease wait unzipping\e[1;31m[+] \e[0m'
+echo ""
+unzip
+echo ""
+echo -e '\e[1;31m [+]\e[0mStarting Web server\e[1;31m[+] \e[0m'
+echo ""
+cd /exec
+php -S localhost:8000
